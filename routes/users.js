a0034a9a1ddd4919d757fc3c0108c705f943b3d5
XanderUZZZER <xanderuzzzer@gmail.com>
2020-08-17 12:27:09 +0300

    backend api done

--- routes/users.js
+++ routes/users.js
@@ -57,7 +57,7 @@ router.post('/', [
         }
       );
     } catch (error) {
-      console.log(error.message);
+      console.error(error.message);
       res.status(500).send('Server error');
     }
   }
