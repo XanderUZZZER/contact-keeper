179316368013aafa5d6a0303c979b2443d967968
XanderUZZZER <xanderuzzzer@gmail.com>
2020-08-19 20:41:50 +0300

    ready to deploy to heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,14 +10,23 @@ connectDB();
 //Init middleware
 app.use(express.json({ extended: false }));///---- watch again!!!!
 
-app.get('/', (req, res) =>
-  res.json({ msg: 'Welcome to tho ContactKeeper API' })
-);
+// app.get('/', (req, res) =>
+//   res.json({ msg: 'Welcome to tho ContactKeeper API' })
+// );
 
 // define routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production'){
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  // look for index.html in currFolder (__dirname)/client/build/index.html
+  app.get('*', (req, res)=>res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
